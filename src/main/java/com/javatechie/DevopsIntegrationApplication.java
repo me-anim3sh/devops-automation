c5fbcbf707514fc993910e7c71346ccb619a02ae
ANIMESH SINGH <me.animesh22@gmail.com>
2024-03-05 00:08:36 -0800

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome :}";
 	}
 
 	public static void main(String[] args) {
